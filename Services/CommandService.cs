e959221d79efc1594d324bcbc5f393aae75a19bc
Bobby Burden III <bobby@brb3.org>
2022-03-27 13:07:32 -0400

    Add some comments to CommandService

--- Services/CommandService.cs
+++ Services/CommandService.cs
@@ -13,6 +13,7 @@ public class CommandService : ICommandService
 
     public CommandService(GifCommand gifCommand, SpongebobCommand spongebobCommand, StallmanCommand stallmanCommand, ILogger<CommandService> logger)
     {
+        // Register new commands here by adding it to the constructor and appending it to the list.
         Commands = new List<Command>()
         {
             gifCommand,
@@ -22,16 +23,35 @@ public class CommandService : ICommandService
         Logger = logger;
     }
 
+    /// <summary>
+    /// Finds the command with the `CommandText` that matches the command from
+    /// the WebhookMessage and calls `Execute` on that command.
+    ///
+    /// If an optional "commandSuffix" is provided (via configuration), that is
+    /// stripped from the message's command before searching the list of commands.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="commandSuffix"></param>
+    /// <returns></returns>
     public async Task HandleMessage(WebhookMessage message, string commandSuffix = "")
     {
-        var messageCommand = message.Command.Substring(1); // Remove leading forward-slash
-        messageCommand = messageCommand.Substring(0, messageCommand.Length - commandSuffix.Length); // Remove any optional suffix
+        // Remove leading forward-slash and any optional suffix
+        var messageCommand = message.Command.Substring(1);
+        messageCommand = messageCommand.Substring(0, messageCommand.Length - commandSuffix.Length);
 
         var command = Commands.Find((c) => c.CommandText == messageCommand);
 
         await command.Execute(message);
     }
 
+    /// <summary>
+    /// Finds the command with the `CommandText` that matches the command from
+    /// the InteractiveMessage and calls `Execute` on that command.
+    ///
+    /// Unlike `HandleMessage`, no suffix is required.
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
     public async Task HandleInteractive(InteractiveMessage message)
     {
         var command = Commands.Find((c) => c.CommandText == message.CallbackId);
