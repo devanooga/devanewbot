b12ea7b89c86684a9680c2d9ea23bff6961cd7eb
William Roush <william.roush@roushtech.net>
2024-05-31 04:00:41 +0000

    Fix seek issue

--- Api/v0/Controllers/EmojiController.cs
+++ Api/v0/Controllers/EmojiController.cs
@@ -1,10 +1,10 @@
 namespace devanewbot.Api.v0.Controllers;
 
+using System;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Mime;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SlackNet;
@@ -28,16 +28,21 @@ public class EmojiController : Controller
     {
         var emojis = await Client.Emoji.List();
         using var memoryStream = new MemoryStream();
-        using var archive = new ZipArchive(memoryStream);
-        using var httpClient = HttpClientFactory.CreateClient();
-        foreach (var emoji in emojis.Where(e => !e.Value.StartsWith("alias:")))
+        using (var archive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
         {
-            var response = await httpClient.GetStreamAsync(emoji.Value);
-            var entry = archive.CreateEntry($"{emoji.Key}.png");
-            var entryStream = entry.Open();
-            await response.CopyToAsync(entryStream);
+            using var httpClient = HttpClientFactory.CreateClient();
+            foreach (var emoji in emojis.Where(e => !e.Value.StartsWith("alias:")))
+            {
+                var response = await httpClient.GetStreamAsync(emoji.Value);
+                var uri = new Uri(emoji.Value);
+                var fileExtension = Path.GetExtension(uri.AbsolutePath);
+                var entry = archive.CreateEntry($"{emoji.Key}{fileExtension}");
+                using var entryStream = entry.Open();
+                await response.CopyToAsync(entryStream);
+            }
         }
 
-        return File(memoryStream, "application/zip", "emoji.zip");
+        memoryStream.Seek(0, SeekOrigin.Begin);
+        return File(memoryStream.ToArray(), "application/zip", "emoji.zip");
     }
 }
