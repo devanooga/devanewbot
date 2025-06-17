f2f5e0d4baa7e49a8556d5af256247c524de974d
William Roush <william.roush@roushtech.net>
2025-06-17 23:44:26 +0000

    Move to JSON model

--- Api/v0/Controllers/HamAlertController.cs
+++ Api/v0/Controllers/HamAlertController.cs
@@ -1,6 +1,7 @@
 namespace devanewbot.Api.v0.Controllers;
 
 using System.Threading.Tasks;
+using Devanewbot.Api.v0.Models.HamAlert;
 using Microsoft.AspNetCore.Mvc;
 using SlackNet;
 using SlackNet.WebApi;
@@ -16,12 +17,12 @@ public class HamAlertController : Controller
     }
 
     [HttpPost("{userId}")]
-    public async Task<IActionResult> Alert(string userId, [FromQuery] string fullCallsign, [FromForm] string callSign, [FromForm] string frequency, [FromForm] string band, [FromForm] string mode, [FromForm] string time)
+    public async Task<IActionResult> Alert(string userId, [FromBody] AlertModel model)
     {
         await Client.Chat.PostMessage(new Message
         {
             Channel = userId,
-            Text = $"HamAlert: {callSign} {frequency} {band} {mode} {time}"
+            Text = $"HamAlert: {model.CallSign} {model.Frequency} {model.Band} {model.Mode} {model.Time}"
         });
 
         return Ok();
---
+++ Api/v0/Models/HamAlert/AlertModel.cs
@@ -0,0 +1,11 @@
+namespace Devanewbot.Api.v0.Models.HamAlert;
+
+public class AlertModel
+{
+    public string? FullCallsign { get; set; }
+    public string? CallSign { get; set; }
+    public string? Frequency { get; set; }
+    public string? Band { get; set; }
+    public string? Mode { get; set; }
+    public string? Time { get; set; }
+}
